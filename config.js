ba8511f3a5b2ba5d157821e7e02961aa9f053433
Abhishek <abhis6585@gmail.com>
2020-08-06 11:41:45 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'secretKey' : '12345-67890-09876-54321',
+    'mongoUrl' : 'mongodb://localhost:27017/userAuth',
+    'facebook' : {
+        clientId: '3462028720483410',
+        clientSecret: '75dcd5400106759bbf0ec1eca2b00ef6'
+    }
+}
