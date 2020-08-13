e1b13fc980de8825ee660dd2f3e15722eb72e9a7
AbhishekDev14 <69293863+AbhishekDev14@users.noreply.github.com>
2020-08-13 13:04:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    'secretKey' : '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/userAuth',
+    'secretKey' : 'Your Secret Key',
+    'mongoUrl' : 'MonogDB url',
     'facebook' : {
-        clientId: '3462028720483410',
-        clientSecret: '75dcd5400106759bbf0ec1eca2b00ef6'
+        clientId: 'FB Client ID',
+        clientSecret: 'Secret key from fb'
     }
 }
